4ff6d99263f346639849925dd1db5ffa9fb31d99
ZierkCodes <zierk.gage@gmail.com>
2021-08-16 11:50:16 -0400

    Added Chat Engine

---
+++ src/pages/VendorChat/VendorChat.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+
+function App() {
+	return (
+		<ChatEngine
+			projectID='aae11ee5-73b1-4fef-9fd5-3d36e5d2c79f'
+			userName='john@smith.co'
+			userSecret='asdfqwer'
+		/>
+	);
+}
+
+export default App;
